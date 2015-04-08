e5268301dd940c13621034ac3d7c93766ac3b276
Eli White <github@eli-white.com>
2015-04-07 23:26:14 -0700

    Adding visualtesting-api to the dependencies

--- visualtesting.conf.js
+++ visualtesting.conf.js
@@ -4,19 +4,19 @@ var Github = require('./node_modules/visualtesting-api/server/utils/github');
 var Differ = require('./node_modules/visualtesting-api/server/utils/differ');
 var storage = require('./node_modules/visualtesting-api/server/utils/storage');
 
-var service;
+var services = [];
 
 if (process.env.GITHUB_BOT_TOKEN !== undefined) {
-  service = new Github({
+  services.push(new Github({
     botToken: process.env.GITHUB_BOT_TOKEN
-  });
+  }));
 }
 
 function Apply(config) {
   config.set({
     port: 9000,
 
-    services: [service],
+    services: services,
 
     differ: Differ,
     storage: storage
